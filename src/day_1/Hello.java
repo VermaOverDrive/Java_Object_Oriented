accef49e8b3b6f8669d91f8dab169aa2b358d2ec
Aditya Verma <vermaaditya646@gmail.com>
2022-09-04 12:55:20 +0530

    commit

--- src/day_1/Hello.java
+++ src/day_1/Hello.java
@@ -2,7 +2,22 @@ package day_1;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World:1");
 
-    }
-}
+        /*DATATYPES AND VARIABLES
+          Interger data types:
+           byte=8bit= 0to256=-128to127
+           short=2byte=16bit=-2^15to(2^15)-1
+           int=4byte=32bit=-2^32to(2^32)-1
+           long=8byte=64bit=-2^64to(2^64)-1
+          Decimal data types :
+           float=
+           double=
+
+         */
+         class DataType{
+            public static void main(String[] args) {
+                float s = 2.3f;
+
+
+            }
